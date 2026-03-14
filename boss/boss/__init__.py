012aadd284a3c38c3489590bbdc143b3394fc656
Haymooed <167770628+Haymooed@users.noreply.github.com>
2026-03-14 19:38:17 +1030

    Update __init__.py

--- boss/boss/__init__.py
+++ boss/boss/__init__.py
@@ -1,24 +1,3 @@
-import logging
-import textwrap
-from typing import TYPE_CHECKING
+from .cog import Boss, setup
 
-from .cog import boss
-
-if TYPE_CHECKING:
-    from ballsdex.core.bot import BallsDexBot
-
-log = logging.getLogger("ballsdex.packages.boss")
-
-LOGO = textwrap.dedent(r"""
-    +---------------------------------------+
-    |      BallsDex Boss Pack v3            |
-    |        Licensed under MIT             |
-    +---------------------------------------+
-""").strip()
-
-
-async def setup(bot: "BallsDexBot"):
-    print(LOGO)
-    log.info("Loading Boss package...")
-    await bot.add_cog(Boss(bot))
-    log.info("Boss package loaded successfully!")
+__all__ = ["Boss", "setup"]
